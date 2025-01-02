2abc935fa46dfafb47e154f50fc23f714b4496be
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-01-02 18:25:15 +0000

    Headline and Body is created

--- MyMobileApp/App.js
+++ MyMobileApp/App.js
@@ -1,15 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { PaperProvider, Text } from 'react-native-paper';
 
 
 export default function App() {
   return (
     <PaperProvider>
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <Text variant="headlineLarge">Headline Large</Text>
+        <Text variant="bodyMedium">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+          took a galley of type and scrambled it to make a type specimen book. It has survived not only five
+          centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+          and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </Text>
+        <StatusBar style="auto" />
+      </View>
     </PaperProvider>
   );
 }
