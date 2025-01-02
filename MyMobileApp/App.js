4ca0081510cf7cee57666301f8141aeb62b2e0ad
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-01-02 18:48:21 +0000

    SafeAreaView is added

--- MyMobileApp/App.js
+++ MyMobileApp/App.js
@@ -1,18 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View , ScrollView} from 'react-native';
-import { PaperProvider, Text , Divider} from 'react-native-paper';
+import { PaperProvider} from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import Home from './components/Home';
 
 
 export default function App() {
   return (
     <PaperProvider>
+      <SafeAreaView>
       <ScrollView>
       <View style={styles.container}>
         <Home/>
         <StatusBar style="auto" />
       </View>
       </ScrollView>
+      </SafeAreaView>
     </PaperProvider>
   );
 }
