f983fd93a7ba875b01806da8e18d5e4fbae7bc33
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-01-02 18:39:26 +0000

    Home component is updated by putting styles

--- MyMobileApp/App.js
+++ MyMobileApp/App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import { PaperProvider, Text } from 'react-native-paper';
+import { PaperProvider, Text , Divider} from 'react-native-paper';
 
 
 export default function App() {
   return (
     <PaperProvider>
       <View style={styles.container}>
-        <Text variant="headlineLarge">Headline Large</Text>
-        <Text variant="bodyMedium">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
-          took a galley of type and scrambled it to make a type specimen book. It has survived not only five
-          centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-          It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-          and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </Text>
         <StatusBar style="auto" />
       </View>
     </PaperProvider>
@@ -27,5 +19,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
--- MyMobileApp/components/Home.js
+++ MyMobileApp/components/Home.js
@@ -1,6 +1,27 @@
-export default function Home(){
-    return(
-        
+import { StyleSheet, View } from 'react-native';
+import { Text, Divider } from 'react-native-paper';
+
+
+export default function Home() {
+    return (
+        <View>
+            <Text variant="headlineLarge">Headline Large</Text>
+            <Divider />
+            <Text variant="bodyMedium" style={styles.body}>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+                took a galley of type and scrambled it to make a type specimen book. It has survived not only five
+                centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </Text>
+        </View>
 
     )
 }
+
+const styles = StyleSheet.create({
+    body: {
+        padding: 8,
+        textAlign: 'justify'
+    }
+});
